5761dfc82ea93fa7c012e099854ba68299de6e8c
Jack Davidson <jackmaverickavidson@gmail.com>
2022-09-07 10:37:54 -0500

    add population entry struct

--- world_data/wdata.cpp
+++ world_data/wdata.cpp
@@ -9,6 +9,14 @@
  */
 
 #include <iostream>
+#include <string>
+
+/* One csv row of mortality rate data. (Non-(2000-20005) populations are ignored).  */
+struct population {
+	int index;
+	std::string region;  /* Area/Region/Country */
+	int population;  /* 2000-2005 */
+};
 
 int main (int argc, char *argv[])
 {
