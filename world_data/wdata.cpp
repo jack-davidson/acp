b5eec5ebfb7d1a9053e86ee5c12b8c671e3cbd41
Jack Davidson <jackmaverickavidson@gmail.com>
2022-09-07 10:30:05 -0500

    add header comment

--- world_data/wdata.cpp
+++ world_data/wdata.cpp
@@ -1,3 +1,13 @@
+/* Author: Jack Davidson
+ *
+ * World Data Lab - Read/Write CSV
+ *
+ * 	- Create an ordered list by longest lived population from 2000-2005 year group.
+ * 	- Where does the United States rank in the list by more developed regions
+ * 		(comprised of Europe, Northern America, Australia/New Zealand and Japan)?
+ * 	- Where is it ranked overall?
+ */
+
 #include <iostream>
 
 int main (int argc, char *argv[])
