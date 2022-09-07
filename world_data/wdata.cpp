a646b2db0bed65446c42326caa12c75bac56bc69
Jack Davidson <jackmaverickavidson@gmail.com>
2022-09-07 11:15:17 -0500

    add tokenizer

--- world_data/wdata.cpp
+++ world_data/wdata.cpp
@@ -9,16 +9,35 @@
  */
 
 #include <iostream>
+#include <sstream>
+#include <fstream>
 #include <string>
+
+using namespace std;
 
 /* One csv row of mortality rate data. (Non-(2000-20005) populations are ignored).  */
 struct population {
 	int index;
-	std::string region;  /* Area/Region/Country */
+	string region;  /* Area/Region/Country */
 	int population;  /* 2000-2005 */
 };
 
 int main (int argc, char *argv[])
 {
+	ifstream f;
+	string r, column;
+
+	f.open("mortality_rates.csv");
+
+	/* every row in csv.  */
+	while(getline(f, r)) {
+		stringstream row(r);
+		while(getline(row, column, ',')) {
+			cout << column;
+		}
+		cout << endl;
+	}
+
+	f.close();
 	return 0;
 }
