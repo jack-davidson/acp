ce99d1dd3a64ab490f245588162ccd5ad8d53b67
Jack Davidson <jackmaverickavidson@gmail.com>
2022-09-16 10:53:13 -0500

    get countries in one array

--- world_data/wdata.cpp
+++ world_data/wdata.cpp
@@ -2,7 +2,7 @@
  *
  * World Data Lab - Read/Write CSV
  *
- * 	- Create an ordered list by longest lived population from 2000-2005 year group.
+ * 	- Create an ordered list by longest lived rate from 2000-2005 year group.
  * 	- Where does the United States rank in the list by more developed regions
  * 		(comprised of Europe, Northern America, Australia/New Zealand and Japan)?
  * 	- Where is it ranked overall?
@@ -16,11 +16,11 @@
 
 using namespace std;
 
-/* One csv row of mortality rate data. (Non-(2000-20005) populations are ignored).  */
-struct population {
+/* One csv row of mortality rate data. (Non-(2000-20005) rates are ignored).  */
+struct rate {
 	int index;
 	string region;  /* Area/Region/Country */
-	float population;  /* 2000-2005 */
+	float rate;  /* 2000-2005 */
 };
 
 int main (int argc, char *argv[])
@@ -29,30 +29,37 @@ int main (int argc, char *argv[])
 	stringstream row;
 	ifstream f;
 	int i;  /* Column number.  */
+	int ri; /* rate index */
 
 	f.open("mortality_rates.csv");
+
+	struct rate rates[241];
+	ri = 0;
 
 	/* Every row in csv.  */
 	while(getline(f, r)) {
 		/* Convert string(r) to stringstream(row) for
 		 * use with a columnar getline() call.  */
 		row = stringstream(r);  
+		if (r[0] == ',' || r[0] == '"') continue;
 
-		struct population p;
+		struct rate *p = &rates[ri++];
 
 		i = 0; /* Reset element number.  */
 		while(getline(row, re, ',')) {
 			switch(i) {
 			case 0: /* index header */
-				p.index = atoi(re.c_str());
+				p->index = atoi(re.c_str());
 			case 2:
-				p.region = re;
+				p->region = re;
 			case 15:
-				p.population = atof(re.c_str());
+				p->rate = atof(re.c_str());
 			}
 			i++;
 		}
-		cout << "index=" << p.index << ", region=" << p.region << ", population=" << p.population << endl;
+	}
+	for (struct rate p: rates) {
+		cout << "index=" << p.index << ", region=" << p.region << ", rate=" << p.rate << endl;
 	}
 
 	f.close();
